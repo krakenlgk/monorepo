2016aee2f0bf9afb0581c4ee1d3a4e3bf8eccad6
karthikeyangs9 <karthikeyan@last9.io>
2025-07-17 20:04:57 +0530

    feat: initialize Next.js frontend application

---
+++ packages/frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['next/core-web-vitals', '../../.eslintrc.js'],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    // Next.js specific rules
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+}
---
+++ packages/frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Monorepo Frontend</h1>
+      <p>This is a Next.js application running in a monorepo setup.</p>
+    </main>
+  )
+}
